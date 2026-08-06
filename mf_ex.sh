aec27d93fc833f5f7331aea33f7f2343f1a4adaa
greeenlaser <sanderveski@gmail.com>
2026-08-06 14:26:39 +0300

    added external shared and export pipeline

---
+++ mf_ex.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+# Move file for use with mf, read more at https://github.com/greeenlaser/personal-stash/tree/main/mf
+
+set -e
+
+#
+# References
+#
+
+case "$1" in
+    --linux)
+        OUT_NAME=KalaAudio-Linux
+
+        LIB_NAME=libkalaaudio
+        LIB_EXT=a
+        LIB_ORIGIN=build/release-linux
+        ;;
+    --win-on-linux)
+        OUT_NAME=KalaAudio-Win-on-Linux
+
+        LIB_NAME=kalaaudio-gnu
+        LIB_EXT=lib
+        LIB_ORIGIN=build/release-win-on-linux
+        ;;
+    --windows)
+        OUT_NAME=KalaAudio-Windows
+
+        LIB_NAME=kalaaudio
+        LIB_EXT=lib
+        LIB_ORIGIN=build/release-windows
+        ;;
+    *)
+        echo "Error: Argument must be --linux, --win-on-linux or --windows" >&2
+        exit 1
+        ;;
+esac
+
+OUT_VER=1-0-0
+OUT_DIR=out/${OUT_NAME}-${OUT_VER}
+
+README=README.md
+LICENSE=LICENSE.md
+INCLUDE=include
+DOCS=docs
+
+DIR_ES=../external-shared
+IN_KH=${DIR_ES}/KalaHeaders
+OUT_KH_NAME=kalaheaders
+
+# MiniAudio
+
+IN_MA=${DIR_ES}/miniaudio
+OUT_MA_NAME=miniaudio
+
+#
+# Core stuff
+#
+
+# Always a fresh start
+mkdir -p "out"
+rm -rf "${OUT_DIR}"
+mkdir "${OUT_DIR}"
+mkdir "${OUT_DIR}/${OUT_KH_NAME}"
+
+# The base files
+mf --f "${README}" --t "${OUT_DIR}/${README}"
+mf --f "${LICENSE}" --t "${OUT_DIR}/${LICENSE}"
+mf --f "${INCLUDE}" --t "${OUT_DIR}"
+mf --f "${DOCS}" --t "${OUT_DIR}"
+
+# The binary
+if [ ! -f "${LIB_ORIGIN}/${LIB_NAME}.${LIB_EXT}" ]; then
+    printf 'Error: Binary %s not found\n' "${LIB_ORIGIN}/${LIB_NAME}.${LIB_EXT}" >&2
+    exit 1
+fi
+
+mf --f "${LIB_ORIGIN}/${LIB_NAME}.${LIB_EXT}" --t "${OUT_DIR}/${LIB_NAME}.${LIB_EXT}"
+
+# KalaHeaders
+mf --f "${IN_KH}/${README}" --t "${OUT_DIR}/${OUT_KH_NAME}/${README}"
+mf --f "${IN_KH}/${LICENSE}" --t "${OUT_DIR}/${OUT_KH_NAME}/${LICENSE}"
+
+mf --f "${IN_KH}/include" --t "${OUT_DIR}/${OUT_KH_NAME}"
+
+# Lua
+mkdir "${OUT_DIR}/${OUT_MA_NAME}"
+
+mf --f "${IN_MA}/${README}" --t "${OUT_DIR}/${OUT_MA_NAME}/${README}"
+mf --f "${IN_MA}/${LICENSE}" --t "${OUT_DIR}/${OUT_MA_NAME}/${LICENSE}"
+
+mf --f "${IN_MA}/include" --t "${OUT_DIR}/${OUT_MA_NAME}"
+
+# MiniAudio binary
+case "$1" in
+    --linux)
+        mf --f "${IN_MA}/release/libminiaudio.a" --t "${OUT_DIR}/${OUT_MA_NAME}/libminiaudio.a"
+        ;;
+    --win-on-linux)
+        mf --f "${IN_MA}/release/miniaudio-gnu.lib" --t "${OUT_DIR}/${OUT_MA_NAME}/miniaudio-gnu.lib"
+        ;;
+    --windows)
+        mf --f "${IN_MA}/release/miniaudio.lib" --t "${OUT_DIR}/${OUT_MA_NAME}/miniaudio.lib"
+        ;;
+esac
